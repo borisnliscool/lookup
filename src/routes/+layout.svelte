91725509655d0597156f6513f023a95c811fdfaf
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2024-01-21 17:50:49 +0100

    feat: add analytics script

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,17 @@
 	import '@fontsource-variable/inter';
 	import '../app.postcss';
 
+	import { dev } from '$app/environment';
 	import SvelteSeo from 'svelte-seo';
 </script>
+
+{#if !dev}
+	<script
+		async
+		src="https://analytics.boris.foo/script.js"
+		data-website-id="f606a422-e919-4c3f-b7eb-941689d8a349"
+	></script>
+{/if}
 
 <main class="min-h-screen h-full bg-neutral-900 text-white">
 	<slot />
