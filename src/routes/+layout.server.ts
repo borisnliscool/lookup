73267f24ab5b219a3b9c0f3cbf864850e9e00e07
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2024-03-04 22:57:39 +0100

    tweak: only get commit every six hours

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -6,23 +6,32 @@ interface Commit {
 	sha: string;
 }
 
-export const load = (async () => {
-	let commit = null;
+let commit: string | undefined;
+let lastUpdate: Date | undefined;
 
-	try {
-		const response = await fetch(
-			'https://api.github.com/repos/borisnliscool/lookup/commits?per_page=1',
-			{
-				headers: {
-					Authorization: `Bearer ${GITHUB_TOKEN}`,
-					'user-agent': 'borisnliscool - lookup'
+export const load = (async () => {
+	if (
+		!commit ||
+		commit == '' ||
+		(lastUpdate && new Date().getTime() - lastUpdate.getTime() > 6 * 60 * 60 * 1000)
+	) {
+		try {
+			const response = await fetch(
+				'https://api.github.com/repos/borisnliscool/lookup/commits?per_page=1',
+				{
+					headers: {
+						Authorization: `Bearer ${GITHUB_TOKEN}`,
+						'user-agent': 'borisnliscool - lookup'
+					}
 				}
-			}
-		);
-		const data: Commit[] = await response.json();
-		commit = data?.[0]?.sha;
-	} catch (err) {
-		console.error(err);
+			);
+
+			const data: Commit[] = await response.json();
+			commit = data?.[0]?.sha;
+			lastUpdate = new Date();
+		} catch (err) {
+			console.error(err);
+		}
 	}
 
 	return { commit, version: packageJson.version };
